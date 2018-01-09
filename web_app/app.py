e74c21484bcfe0e24f5102d8086b16b221fb9fb8
Ahmad Harminto <ahmad.harminto@sociolabs.io>
2018-01-09 11:38:48 +0700

    add sqlalchemy ORM

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, Column, String
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('settings.py')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    db = SQLAlchemy(app)
+
+    class Page(db.Model):
+        __tablename__ = 'page'
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        content = Column(String)
+
+    class Post(db.Model):
+        __tablename__ = 'post'
+        id = Column(Integer, primary_key=True)
+        content = Column(String)
+
+    db.create_all()
 
     @app.route('/')
     def index():
@@ -17,12 +34,12 @@ def create_app():
     def db_test():
         import psycopg2
 
-        connection = psycopg2.connect('dbname=flask user=postgres password=postgres host=postgresql')
+        connection = psycopg2.connect('dbname=flask user=postgres password=postgres host=postgres')
         cursor = connection.cursor()
         cursor.execute('select * from page')
-        id, title = cursor.fetchone()
+        id, title, content = cursor.fetchone()
         connection.close()
 
-        return 'Test query fetch one : {} | {}'.format(id, title)
+        return 'Test query fetch 1 : {} | {}'.format(id, title)
 
     return app
--- web_app/settings.py
+++ web_app/settings.py
@@ -1,1 +1,4 @@
-DEBUG = True
+DEBUG=True
+SECRET_KEY='base64:l/QPDDliJEA/0p/cyJgaIY8eGmCcXY+fia/D5y52H8c='
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:postgres@postgres:5432/flask'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
